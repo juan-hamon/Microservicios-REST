545fe05aa89d845511c92de9bfce3923f825b6cc
juan-hamon <53021906+juan-hamon@users.noreply.github.com>
2021-09-10 20:26:25 -0500

    Desarrollo de operaciones de suma y resta para la parte 2

--- Parte 2/sumador/src/main/java/javeriana/ms/sumador/MySumController.java
+++ Parte 2/sumador/src/main/java/javeriana/ms/sumador/MySumController.java
@@ -1,4 +1,14 @@
-package main.java.javeriana.ms.sumador;
+package javeriana.ms.sumador;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -8,11 +18,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MySumController {
+    
     @Autowired
     Environment environment;
 
+    private static final String FILE_ROUTE = "src/JsonFile/database.json";
+
+    public JSONArray readJson(){
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(FILE_ROUTE))
+        {
+            Object obj = jsonParser.parse(reader);
+            JSONArray information = (JSONArray) obj;
+            return information;
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (org.json.simple.parser.ParseException e) {
+            return new JSONArray();
+        }
+        return new JSONArray();
+    }
+
+    public void writeJSON(JSONArray newInfo){
+        try (FileWriter file = new FileWriter(FILE_ROUTE)) {
+            file.write(newInfo.toJSONString()); 
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/suma/files")
+    public JSONArray sumHistory(){
+        return readJson();
+    }
+
     @GetMapping("/suma")
-    public Integer sum(@RequestParam int a, @RequestParam int b){
-        return a+b;
+    public Integer sum(@RequestParam int a, @RequestParam int b, @RequestParam String name){
+        Integer result = a+b;
+        HashMap<String,String> operationDetails = new HashMap<>();
+        operationDetails.put("Operando1", String.valueOf(a));
+        operationDetails.put("Operando2", String.valueOf(b));
+        operationDetails.put("Resultado", String.valueOf(result));
+        operationDetails.put("Usuario", name);
+        operationDetails.put("Fecha", LocalDate.now().toString());
+        JSONArray values = readJson();
+        values.add(operationDetails);
+        writeJSON(values);
+        return result;
     }
 }
--- Parte 2/sustractor/src/main/java/javeriana/ms/sustractor/MySubtractController.java
+++ Parte 2/sustractor/src/main/java/javeriana/ms/sustractor/MySubtractController.java
@@ -1,4 +1,14 @@
-package main.java.javeriana.ms.sustractor;
+package javeriana.ms.sustractor;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -11,8 +21,52 @@ public class MySubtractController {
     @Autowired
     Environment environment;
 
+    private static final String FILE_ROUTE = "src/JsonFile/database.json";
+
+    public JSONArray readJson(){
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(FILE_ROUTE))
+        {
+            Object obj = jsonParser.parse(reader);
+            JSONArray information = (JSONArray) obj;
+            return information;
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (org.json.simple.parser.ParseException e) {
+            return new JSONArray();
+        }
+        return new JSONArray();
+    }
+
+    public void writeJSON(JSONArray newInfo){
+        try (FileWriter file = new FileWriter(FILE_ROUTE)) {
+            file.write(newInfo.toJSONString()); 
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/resta/files")
+    public JSONArray subtractHistory(){
+        return readJson();
+    }
+
     @GetMapping("/resta")
-    public Integer subtract(@RequestParam int a, @RequestParam int b){
-        return a-b;
+    public Integer subtract(@RequestParam int a, @RequestParam int b, @RequestParam String name){
+        Integer result = a-b;
+        HashMap<String,String> operationDetails = new HashMap<>();
+        operationDetails.put("Operando1", String.valueOf(a));
+        operationDetails.put("Operando2", String.valueOf(b));
+        operationDetails.put("Resultado", String.valueOf(result));
+        operationDetails.put("Usuario", name);
+        operationDetails.put("Fecha", LocalDate.now().toString());
+        JSONArray values = readJson();
+        values.add(operationDetails);
+        writeJSON(values);
+        return result;
     }
 }
